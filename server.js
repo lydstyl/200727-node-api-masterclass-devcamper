14cc7aefd311731ddc0e9f1f4085e31277fb80fa
Gabriel Brun <lydstyl@gmail.com>
2020-08-11 15:44:26 +0200

    express-fileupload & upload photo

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
-const errorHandler = require('./middlewares/error')
 const colors = require('colors')
+const fileupload = require('express-fileupload')
+const errorHandler = require('./middlewares/error')
 const connectDB = require('./config/db')
 
 // Load env vars
@@ -25,6 +27,13 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
+// File uploading
+app.use(fileupload())
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Mount routers
 app.use('/api/v1/bootcamps', bootcamps)
 app.use('/api/v1/courses', courses)
 app.use(errorHandler)
