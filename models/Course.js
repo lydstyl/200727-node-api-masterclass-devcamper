792688ed057b8d44badfa4ad318fb5e10f8d09e1
Gabriel Brun <lydstyl@gmail.com>
2020-08-18 12:36:26 +0200

    Aggregate - Calculate Average Rating & add Review

--- models/Course.js
+++ models/Course.js
@@ -68,12 +68,12 @@ CourseSchema.statics.getAverageCost = async function (bootcampId) {
   }
 }
 
-// Gall getAverageCost after save
+// Call getAverageCost after save
 CourseSchema.post('save', function () {
   this.constructor.getAverageCost(this.bootcamp)
 })
 
-// Gall getAverageCost before remove
+// Call getAverageCost before remove
 CourseSchema.pre('remove', function () {
   this.constructor.getAverageCost(this.bootcamp)
 })
