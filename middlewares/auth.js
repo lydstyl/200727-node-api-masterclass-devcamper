65ec86113d262f326a3ad73f5d316e8342a935bb
Gabriel Brun <lydstyl@gmail.com>
2020-08-19 15:26:28 +0200

    Logout to clear token cookie

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -11,16 +11,15 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
-    token = req.headers.authorization.split(' ')[1]
+    token = req.headers.authorization.split(' ')[1] // Set token from Bearer in header
   }
-
-  // else if(req.cookies.token){
-  //   token = req.cookies.token
+  // else if (req.cookies.token) {
+  //   token = req.cookies.token // Set token from cookie
   // }
 
   // Make sure token exists
   if (!token) {
-    return next(new ErrorResponse('Not authorize to access this route', 401))
+    return next(new ErrorResponse('Not authorized to access this route', 401))
   }
 
   try {
@@ -31,7 +30,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
 
     next()
   } catch (error) {
-    return next(new ErrorResponse('Not authorize to access this route', 401))
+    return next(new ErrorResponse('Not authorized to access this route', 401))
   }
 })
 
